1d209fc87c32dbf9890e153f4fb63de59e9bec5c
querry43 <querry43@0215a878-da9f-11dd-93da-dbb4cac96d7c>
2009-01-04 22:06:09 +0000

    first commit
    git-svn-id: http://legonxtremote.googlecode.com/svn/trunk@2 0215a878-da9f-11dd-93da-dbb4cac96d7c

---
+++ src/main.m
@@ -0,0 +1,16 @@
+/* $Id$ */
+
+//
+//  main.m
+//  NXT
+//
+//  Created by Matt Harrington on 01/04/09.
+//  Copyright underdogma.net 2009. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
